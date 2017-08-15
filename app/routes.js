477f1c74634360b06ef0469e4d425463ca8432f8
Mustafa <mustafa05deniz@gmail.com>
2017-08-15 11:04:06 +0300

    Update routes.js

--- app/routes.js
+++ app/routes.js
@@ -13,7 +13,7 @@ module.exports = function(app) {
   
 
     var corsOptions = {
-        origin: 'http://project.kodizim.com:3000/',
+        origin: 'http://localhost:3000/',
         optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
     }
 
